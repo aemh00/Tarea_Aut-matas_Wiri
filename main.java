c2f98e66fda96450477ca66d5ceb3ccb23f934ec
kokee07 <gamerjorge@gmail.com>
2017-06-13 00:00:25 -0400

    Main Update

--- main.java
+++ main.java
@@ -1,10 +1,8 @@
 import java.util.*; 
 import javax.swing.*; 
-@SuppressWarnings("serial") 
 public class main extends Window { 
   
   public static void main(String[] args) { 
-    //Ventana win = new Ventana(); 
     Window win = new Window(); 
   }
 } 
